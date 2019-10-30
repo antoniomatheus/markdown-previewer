87cb6741b70d3919f963890c95f62443c30de02c
Antonio Matheus Silva <antonimathsilva@gmail.com>
2019-10-30 11:25:04 -0300

    more styling

--- src/components/Markdown/styles.js
+++ src/components/Markdown/styles.js
@@ -7,17 +7,20 @@ export const Container = styled.div`
   flex-direction: column;
 
   header {
+    font-family: 'Audiowide', cursive;
     padding: 10px;
     text-align: center;
     font-weight: bold;
     font-size: 24px;
-    background-color: #65b54a;
+    background-color: #573fa8;
+    color: #fff;
     border-bottom: 1px solid #000;
   }
 
   textarea {
+    border: none;
     box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
-    background: rgba(101, 181, 74, 0.1);
+    background: rgba(255, 255, 255, 0.7);
     padding: 10px;
     border-radius: 4px;
     width: 90%;
--- src/styles.js
+++ src/styles.js
@@ -7,5 +7,5 @@ export const Container = styled.div`
   max-width: 1980px;
   justify-content: stretch;
   align-items: stretch;
-  background: rgba(101, 181, 74, 0.4);
+  background: #7159c1;
 `;
