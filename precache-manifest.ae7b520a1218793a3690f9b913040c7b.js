11b1e9449cf577a9001039b0af824dc99c0d2cd5
Antonio Matheus Silva <antonimathsilva@gmail.com>
2019-10-30 12:00:00 -0300

    Updates

---
+++ precache-manifest.ae7b520a1218793a3690f9b913040c7b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d182e77fc8c7e08863510af5437e1e44",
+    "url": "/github.io/markdown-previewer/index.html"
+  },
+  {
+    "revision": "951ad2ffa909e37070df",
+    "url": "/github.io/markdown-previewer/static/js/2.93ade280.chunk.js"
+  },
+  {
+    "revision": "5343a42d08956f67a15b",
+    "url": "/github.io/markdown-previewer/static/js/main.b328fd3a.chunk.js"
+  },
+  {
+    "revision": "1a49d0d9f2fc61b803c9",
+    "url": "/github.io/markdown-previewer/static/js/runtime-main.415d3087.js"
+  }
+]);
